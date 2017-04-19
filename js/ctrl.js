1dc87194bc894f374a0005ed4f666f5c2070b296
Ashok Reddy <ashokcse505@gmail.com>
2017-04-19 12:59:36 +0530

    Comapre Code Push

---
+++ js/ctrl.js
@@ -0,0 +1,17 @@
+(function(){
+    
+    
+    $("#compareTable").hide();
+      $("#footer").css("margin-top", "398px");
+    
+    
+    $("#btnSearch").click(function(){
+    $("#compareTable").show();
+        $("#footer").css("margin-top", "auto");
+});
+    
+    
+    
+    
+    
+})();
